074d99779585bb7a0b62b076b0b9adb051e908e6
Mark Witt <hello@markwitt.me>
2021-02-03 14:35:34 +0100

    better interface + readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Usage
+
+1. Run `npm install` (assuming Node.js is installed) inside the project directory.
+2. If running the first time, run `node precision.js` to get the market precisions for all coins (to avoid rounding errors).
+3. Put your Binance API keys into a .env file in the project directory (take a look at .env.example). Make sure your API Key has sufficient permissions to trade.
+4. run `npm start` to run the script. Enter the amount of BTC to invest and the coin you want to buy.
+5. ...
+6. Profit
+
+Pull requests are welcome. Please follow me on Twitter [@markwitt_me](https://twitter.com/markwitt_me).
--- app.js
+++ app.js
@@ -16,30 +16,38 @@ const binance = new Binance().options({
 });
 
 binance.balance((error, balances) => {
-  if (error) return console.error(error);
+  if (error) return console.error(error.body);
   console.info("BTC balance: ", balances.BTC.available);
-  rl.question("Coin Name?", async (answer) => {
-    const coin = answer.toUpperCase();
-    const precision = precisions[`${coin}BTC`].toString();
-    console.log("Precision: ", precision);
-    binance.prices(`${coin}BTC`, async (error, ticker) => {
-      console.info(`Price of ${coin} : `, ticker[`${coin}BTC`]);
-      if (ticker[`${coin}BTC`]) {
-        const btcAmount = /* balances.BTC.available/5 */ 0.011;
-        const coinAmount = btcAmount / ticker[`${coin}BTC`];
-        const coinAmountRounded =
-          Math.floor(coinAmount / precision) * precision;
-        console.info(`ca. ${btcAmount} BTC => ${coinAmountRounded} ${coin}`);
-        binance
-          .marketBuy(`${coin}BTC`, coinAmountRounded)
-          .then(console.info)
-          .catch((err) => {
-            const errJson = err.toJSON();
-            console.error(errJson);
-          });
-      }
-    });
-
-    rl.close();
+  rl.question("BTC investment?", async (answer) => {
+    const btcAmount = parseFloat(answer);
+    if (btcAmount < balances.BTC.available) {
+      rl.question("Coin Name?", async (answer) => {
+        const coin = answer.toUpperCase();
+        const precision = precisions[`${coin}BTC`].toString();
+        console.log("Precision: ", precision);
+        binance.prices(`${coin}BTC`, async (error, ticker) => {
+          console.info(`Price of ${coin} : `, ticker[`${coin}BTC`]);
+          if (ticker[`${coin}BTC`]) {
+            const coinAmount = btcAmount / ticker[`${coin}BTC`];
+            const coinAmountRounded =
+              Math.floor(coinAmount / precision) * precision;
+            console.info(
+              `ca. ${btcAmount} BTC => ${coinAmountRounded} ${coin}`
+            );
+            binance
+              .marketBuy(`${coin}BTC`, coinAmountRounded)
+              .then(console.info)
+              .catch((err) => {
+                const errJson = err.toJSON();
+                console.error(errJson);
+              });
+          }
+        });
+        rl.close();
+      });
+    } else {
+      console.log("Not enough balance");
+      rl.close();
+    }
   });
 });
